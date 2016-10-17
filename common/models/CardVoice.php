2e51f479b00dd50b417430bcf15f189a914477ec
Dmitry <skaywar4@gmail.com>
2016-10-18 00:44:00 +0700

    asd

--- common/models/CardVoice.php
+++ common/models/CardVoice.php
@@ -64,7 +64,7 @@ class CardVoice extends \yii\db\ActiveRecord
 
     public function getCategory()
     {
-        return $this->hasOne(Category::className(), ['id' => 'category_id'])->inverseOf('name');
+        return $this->hasOne(Category::className(), ['id' => 'category_id']);
     }
 
     /**
