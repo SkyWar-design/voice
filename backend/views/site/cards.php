0e9d2fe6eacbadffb2beb585a3a86c087856f43e
Dmitry <skaywar4@gmail.com>
2016-10-18 00:19:36 +0700

    asd

--- backend/views/site/cards.php
+++ backend/views/site/cards.php
@@ -45,6 +45,7 @@ $this->title = 'Карточки';
             'voice_text_h1',
             'voice_text_description',
             'voice_text_theme',
+            'name',
             [
                 'class' => 'yii\grid\ActionColumn',
                 'header'=>'Пол',
