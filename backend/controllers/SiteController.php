8732eaa679164c87c3621ee5df91d3a836839977
admin <admin@SMSAERO.local>
2016-10-18 12:41:15 +0600

    asd

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -115,8 +115,7 @@ class SiteController extends Controller
             'models' => $model1,
             'columns' => [
             'id','url','mp3_id','voice_description','voice_keywords','voice_title',
-            'voice_text_h1','voice_text_description','voice_text_theme','voice_text_tags',
-            'category.name','voice_date','sex','status'
+            'voice_text_h1'
             ],
         ]);
 
