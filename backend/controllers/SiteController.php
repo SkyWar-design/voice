2dd4d36341f0254a2f3a8f6812df0c6d4d8c6bc8
admin <admin@SMSAERO.local>
2016-10-18 11:38:28 +0600

    asd

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -108,13 +108,11 @@ class SiteController extends Controller
             'models' => $dataProvider,
             'mode' => 'export', //default value as 'export'
             'columns' => ['id','voice_title'], //without header working, because the header will be get label from attribute label.
-            'header' => ['id' => 'Header Column 1','voice_title' => 'Header Column 2'],
         ]);
 
         \moonland\phpexcel\Excel::export([
             'models' => $dataProvider,
             'columns' => ['id','voice_title'], //without header working, because the header will be get label from attribute label.
-            'header' => ['id' => 'Header Column 1','voice_title' => 'Header Column 2'],
         ]);
 
 
