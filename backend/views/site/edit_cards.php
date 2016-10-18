faf7dec31b5cb345d7e6f5c4e79db898d6b6a185
Dmitry <skaywar4@gmail.com>
2016-10-18 20:26:17 +0700

    123

--- backend/views/site/edit_cards.php
+++ backend/views/site/edit_cards.php
@@ -146,7 +146,7 @@ $this->title = 'Заполнение карточек';
                 <div class="ln_solid"></div>
                 <div class="form-group">
                     <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
-                        <button type="submit" id="send_form" class="btn btn-primary btn-lg">Сохранить и загрузить следующую</button>
+                        <button type="submit" id="send_form" class="btn btn-primary">Сохранить и загрузить следующую</button>
                         <button type="submit" id="del_form" class="btn btn-danger">Удалить</button>
                     </div>
                 </div>
