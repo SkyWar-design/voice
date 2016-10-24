0fa0dcc15c1e71881adbd18353c6c20188a84be0
Dmitry <skaywar4@gmail.com>
2016-10-25 00:54:50 +0700

    21d

--- backend/views/site/edit_cards.php
+++ backend/views/site/edit_cards.php
@@ -41,7 +41,7 @@ $this->title = 'Заполнение карточек';
         </div>
         <div class="x_content">
             <br />
-            <form  id="demo-form2" class="form-horizontal form-label-left" novalidate>
+            <form  id="demo-form2" onsubmit="return false;" class="form-horizontal form-label-left" novalidate>
                 <div class="form-group">
                     <label class="control-label col-md-3 col-sm-3 col-xs-12" for="last-name">MP3 ID<span class="required">*</span>
                     </label>
@@ -137,9 +137,6 @@ $this->title = 'Заполнение карточек';
 
         var token = $('meta[name=csrf-token]').attr("content");
 
-        $('#demo-form2').click(function (){
-           return false;
-        });
         function goPage(url) {
             document.location.href = url;
             return false;
