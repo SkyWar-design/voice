c48921821b0ff2e15cce68327902f16202cbbd96
Dmitry <skaywar4@gmail.com>
2016-11-27 18:58:57 +0600

    123

--- frontend/views/site/calendar.php
+++ frontend/views/site/calendar.php
@@ -9,9 +9,19 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-about">
        <div class="calendar-image"></div>
+
        <div class="caption">
              <h1><?= Html::encode($this->title) ?></h1>
        </div>
 
+        <div class="line1"></div>
 
+        <ul>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+        </ul>
+
+        <div class="line2"></div>
 </div>
