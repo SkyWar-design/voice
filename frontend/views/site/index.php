00bca3e22435d5d9bf9588c06ad5f4e8f47cda8e
admin <admin@SMSAERO.local>
2016-12-05 17:29:05 +0600

    asd

---
+++ frontend/views/site/index.php
@@ -0,0 +1,5 @@
+<div id="smsaero_widget"></div>
+<script src='http://smsaero.ru/service/widget/js/P6lk9Tzduzm3TcCWkyrZwvo6It8BQX5C' type='text/javascript'></script>
+<script>
+    SMSAERO_WIDGET.init("smsaero_widget");
+</script>
