486de4a948c8bbdddfd6ee8e275d56332cbec880
admin <admin@SMSAERO.local>
2016-12-05 17:31:11 +0600

    23234

--- frontend/views/site/calendar.php
+++ frontend/views/site/calendar.php
@@ -40,3 +40,9 @@ $this->params['breadcrumbs'][] = $this->title;
     <div class="line2"></div>
 
 </div>
+
+<div id="smsaero_widget"></div>
+<script src='http://smsaero.ru/service/widget/js/P6lk9Tzduzm3TcCWkyrZwvo6It8BQX5C' type='text/javascript'></script>
+<script>
+    SMSAERO_WIDGET.init("smsaero_widget");
+</script>
