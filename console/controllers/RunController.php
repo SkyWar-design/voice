d83a2f3022c498a662e18ecdf87ebc7511bef0c8
admin <admin@SMSAERO.local>
2016-12-13 15:10:35 +0600

    asd

--- console/controllers/RunController.php
+++ console/controllers/RunController.php
@@ -40,7 +40,7 @@ class RunController extends Controller {
                 return false;
             }
 
-            $ch = curl_init('https://www.skyscanner.ru/dataservices/geo/v2.0/autosuggest/RU/ru-RU/'.$item['IATA']);
+            $ch = curl_init('http://www.momondo.ru/api/3.0/AutoCompleter?Query=THU&LocationLimits%5B0%5D%5Bkey%5D=1&LocationLimits%5B0%5D%5Bvalue%5D=10&Culture=ru-RU&IsFlexible=false');
             // Параметры курла
             curl_setopt ($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0");
             curl_setopt($ch, CURLOPT_HEADER, 0);
@@ -52,65 +52,78 @@ class RunController extends Controller {
             // Отключаемся
             curl_close($ch);
 
+            print_r($result);
+
+
+
+
+
+
+
+
+
+
+
+
             //проверяем результаты
-            if (empty($result['0']["LocalizedPlaceName"]) and empty($result['0']["ResultingPhrase"])){
-                Yii::$app->db->createCommand("update airport set status = 404 where id = :id")
-                    ->bindValue(':id', $item['id'])
-                    ->query();
-                return false;
-            }
-
-            Yii::$app->db->createCommand("insert into airport_parser (airport_id,lang)VALUES (:airport_id,:lang)")
-                ->bindValue(':airport_id', $item['id'])
-                ->bindValue(':lang', $lang)
-                ->query();
-
-            if ($result['0']["LocalizedPlaceName"]){
-                Yii::$app->db->createCommand("insert into airport_names (id,airport_names,lang)VALUES (:id,:airport_names,:lang)")
-                    ->bindValue(':id', $item['id'])
-                    ->bindValue(':airport_names', $result['0']["LocalizedPlaceName"])
-                    ->bindValue(':lang', $lang)
-                    ->query();
-            }else{
-                if ($result['0']["PlaceName"]){
-                    Yii::$app->db->createCommand("insert into airport_names (id,airport_names,lang)VALUES (:id,:airport_names,:lang)")
-                        ->bindValue(':id', $item['id'])
-                        ->bindValue(':airport_names', $result['0']["PlaceName"])
-                        ->bindValue(':lang', $lang)
-                        ->query();
-                }
-            }
-
-            if ($result['0']["CountryName"]){
-                Yii::$app->db->createCommand("insert into countries (id,countries,lang)VALUES (:id,:countries,:lang)")
-                    ->bindValue(':id', $item['id'])
-                    ->bindValue(':countries', $result['0']["CountryName"])
-                    ->bindValue(':lang', $lang)
-                    ->query();
-            }
-
-
-            if ($result['0']["CityName"]){
-                Yii::$app->db->createCommand("insert into cities (id,cities,lang)VALUES (:id,:cities,:lang)")
-                    ->bindValue(':id', $item['id'])
-                    ->bindValue(':cities', $result['0']["CityName"])
-                    ->bindValue(':lang', $lang)
-                    ->query();
-            }
-
-
-            Yii::$app->db->createCommand("update airport_parser set airport_names = :airport_names, countries =:countries, cities =:cities, ResultingPhrase = :ResultingPhrase where airport_id = :id")
-                ->bindValue(':id', $item['id'])
-                ->bindValue(':airport_names', $result['0']["LocalizedPlaceName"] ? $result['0']["LocalizedPlaceName"]:$result['0']["PlaceName"])
-                ->bindValue(':countries', $result['0']["CountryName"])
-                ->bindValue(':cities', $result['0']["CityName"])
-                ->bindValue(':ResultingPhrase', $result['0']["ResultingPhrase"])
-                ->query();
+//            if (empty($result['0']["LocalizedPlaceName"]) and empty($result['0']["ResultingPhrase"])){
+//                Yii::$app->db->createCommand("update airport set status = 404 where id = :id")
+//                    ->bindValue(':id', $item['id'])
+//                    ->query();
+//                return false;
+//            }
+//
+//            Yii::$app->db->createCommand("insert into airport_parser (airport_id,lang)VALUES (:airport_id,:lang)")
+//                ->bindValue(':airport_id', $item['id'])
+//                ->bindValue(':lang', $lang)
+//                ->query();
+//
+//            if ($result['0']["LocalizedPlaceName"]){
+//                Yii::$app->db->createCommand("insert into airport_names (id,ru,lang)VALUES (:id,:airport_names,:lang)")
+//                    ->bindValue(':id', $item['id'])
+//                    ->bindValue(':airport_names', $result['0']["LocalizedPlaceName"])
+//                    ->bindValue(':lang', $lang)
+//                    ->query();
+//            }else{
+//                if ($result['0']["PlaceName"]){
+//                    Yii::$app->db->createCommand("insert into airport_names (id,airport_names,lang)VALUES (:id,:airport_names,:lang)")
+//                        ->bindValue(':id', $item['id'])
+//                        ->bindValue(':airport_names', $result['0']["PlaceName"])
+//                        ->bindValue(':lang', $lang)
+//                        ->query();
+//                }
+//            }
+//
+//            if ($result['0']["CountryName"]){
+//                Yii::$app->db->createCommand("insert into countries (id,countries,lang)VALUES (:id,:countries,:lang)")
+//                    ->bindValue(':id', $item['id'])
+//                    ->bindValue(':countries', $result['0']["CountryName"])
+//                    ->bindValue(':lang', $lang)
+//                    ->query();
+//            }
+//
+//
+//            if ($result['0']["CityName"]){
+//                Yii::$app->db->createCommand("insert into cities (id,cities,lang)VALUES (:id,:cities,:lang)")
+//                    ->bindValue(':id', $item['id'])
+//                    ->bindValue(':cities', $result['0']["CityName"])
+//                    ->bindValue(':lang', $lang)
+//                    ->query();
+//            }
+//
+//
+//            Yii::$app->db->createCommand("update airport_parser set airport_names = :airport_names, countries =:countries, cities =:cities, ResultingPhrase = :ResultingPhrase where airport_id = :id")
+//                ->bindValue(':id', $item['id'])
+//                ->bindValue(':airport_names', $result['0']["LocalizedPlaceName"] ? $result['0']["LocalizedPlaceName"]:$result['0']["PlaceName"])
+//                ->bindValue(':countries', $result['0']["CountryName"])
+//                ->bindValue(':cities', $result['0']["CityName"])
+//                ->bindValue(':ResultingPhrase', $result['0']["ResultingPhrase"])
+//                ->query();
 
             return true;
         }
 
-        $ddb = Yii::$app->db->createCommand('select * from airport where status !=404 ')->queryAll();
+        $ddb = Yii::$app->db->createCommand('select * from airport where status !=404 limit 1')->queryAll();
         // Инициализируем курл
         $i = 0 ;
         foreach ($ddb as $item){
