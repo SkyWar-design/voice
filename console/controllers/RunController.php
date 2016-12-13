301396b77fe23cf55398459e0e2313dbab135761
Dmitry <skaywar4@gmail.com>
2016-12-13 21:05:11 +0600

    фывф

--- console/controllers/RunController.php
+++ console/controllers/RunController.php
@@ -160,8 +160,8 @@ class RunController extends Controller {
 
 
         $langs = [
-            '0'  => ['lang' => 'zh-CN', 'code'=>'zh'],// Китайский zh-CN zh
-            '1'  => ['lang' => 'fr-FR', 'code'=>'fr'],// Французский fr-FR fr
+            '0'  => ['lang' => 'zh-CN', 'code'=>'zh'],// Китайский zh-CN zh       ГОТОВО
+            '1'  => ['lang' => 'fr-FR', 'code'=>'fr'],// Французский fr-FR fr     ГОТОВО
             '2'  => ['lang' => 'es-ES', 'code'=>'es'],// Испанский es-ES es
             '3'  => ['lang' => 'de-DE', 'code'=>'de'],// Немецкий de-DE de
             '4'  => ['lang' => 'pt-PT', 'code'=>'pt'],// Португальский pt-PT pt
@@ -176,7 +176,7 @@ class RunController extends Controller {
         foreach ($ddb as $item){
             $i++;
             print_r($i);
-            if(go_parse($item, $langs[1]))
+            if(go_parse($item, $langs[2]))
             {
 
             }else{
