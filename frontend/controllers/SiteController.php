fe92472bc4ec30daa2586af0e898c2cb76eedaab
Sergey <s.osipov@smsaero.ru>
2016-12-22 17:54:00 +0600

    test

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -102,7 +102,7 @@ class SiteController extends Controller
     }
 
     //генерируем уникальный хэш для localstorage что бы запоминать пользователь на уровне браузера и в дальнейшем перенести пользователь со статистикой в личный кабинет
-    public function actionGetUserHash(){
+    public function actionUserHash(){
         if( Yii::$app->request->isAjax ){
             $hash = '';
             $random_letter = array_merge(range('A','Z'),range('a','z'),range('0','9'));
