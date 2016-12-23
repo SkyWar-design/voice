85616f0c5b2d7fc6cd55b0880da573b6f48a662d
Sergey <s.osipov@smsaero.ru>
2016-12-23 20:36:13 +0600

    test

--- frontend/views/site/_category_card.php
+++ frontend/views/site/_category_card.php
@@ -1,6 +1,5 @@
 <?php
 use yii\helpers\Url;
-var_dump($model->page);
 ?>
 <div class="player-min-content d-flex-s-b">
     <div class="player-min">
@@ -8,7 +7,7 @@ var_dump($model->page);
         <span class="small-text w-100-p text-center inline">слушать</span>
     </div>
     <div class="card-preview">
-        <a href="<?=Url::toRoute(['site/card', 'id' => $model->id]) ?>" class="card-title m-b-12">А куда делись названия?)</a>
+        <a href="<?=Url::toRoute(['site/card', 'id' => $model->id]) ?>" class="card-title m-b-12"><?=$model->page->title ?></a>
         <a href="<?=Url::toRoute(['site/card', 'id' => $model->id]) ?>" class="button-yellow l-h-30">Отправить</a>
     </div>
     <a class="link-to-category small-text <?=$this->context->main_category['css_style'] ?>" href="<?=Url::toRoute(['site/category', 'id' => $this->context->main_category['id']]) ?>"><?=$this->context->main_category['name'] ?></a>
