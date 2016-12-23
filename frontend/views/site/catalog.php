6593072a48bbb117dec91b6b27b42817eb5fdd32
Sergey <s.osipov@smsaero.ru>
2016-12-23 12:04:14 +0600

    test

--- frontend/views/site/catalog.php
+++ frontend/views/site/catalog.php
@@ -10,10 +10,10 @@ $this->params['breadcrumbs'][] = $this->title;
 <section id="catalog">
     <?php foreach( $categories as $main_category_id => $category_subcategory ): ?>
         <div class="catalog-category">
-            <a href="<?=Url::toRoute('site/catalog/'.$main_category_id) ?>"><h2 class="<?=$css_style_categories[$main_category_id] ?>-big"><?=$category_subcategory['name_category'] ?></h2></a>
+            <a href="<?=Url::toRoute('site/category/'.$main_category_id) ?>"><h2 class="<?=$css_style_categories[$main_category_id] ?>-big"><?=$category_subcategory['name_category'] ?></h2></a>
             <div class="sub-category">
                 <?php foreach( $category_subcategory['subcategories'] as $subcategories_id => $subcategories  ): ?>
-                    <a href="<?=Url::toRoute('site/catalog/'.$subcategories_id) ?>"><?=$subcategories ?></a>
+                    <a href="<?=Url::toRoute('site/category/'.$subcategories_id) ?>"><?=$subcategories ?></a>
                 <?php endforeach; ?>
             </div>
             <hr class="yellow-line m-t-30">
