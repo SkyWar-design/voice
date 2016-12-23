15545130684661b5a0b45bee5ed91e42a6eb950d
Sergey <s.osipov@smsaero.ru>
2016-12-23 18:10:12 +0600

    test

--- frontend/views/site/_category_card.php
+++ frontend/views/site/_category_card.php
@@ -3,7 +3,7 @@ use yii\helpers\Url;
 ?>
 <div class="player-min-content d-flex-s-b">
     <div class="player-min">
-        <audio src="/mp3/<?=$model->mp3_id ?>/.mp3" class="min"></audio>
+        <audio src="/mp3/<?=$model->mp3_id ?>.mp3" class="min"></audio>
         <span class="small-text w-100-p text-center inline">слушать</span>
     </div>
     <div class="card-preview">
