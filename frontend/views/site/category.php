e318458b9856648baf419a340769bed31f72a22e
Sergey <s.osipov@smsaero.ru>
2016-12-24 22:21:35 +0600

    test

--- frontend/views/site/category.php
+++ frontend/views/site/category.php
@@ -24,8 +24,8 @@ $request = Yii::$app->request->get();
         <div class="filter">
             <a href="#" class="button-grey filter-type <?=(!isset($request['card_voice']['sex'])?' max-grey':'') ?>" data-filter="all">Показать все</a>
             <?=(isset($request['card_voice']['sex'])?'<input type="hidden" name="card_voice[sex]" value="'.$request['card_voice']['sex'].'">':'') ?>
-            <a href="#" class="button-grey filter-type<?=($request['card_voice']['sex']==2?' max-grey':'') ?>"  data-filter="card_voice[sex]" data-val="2">Для девушек</a>
-            <a href="#" class="button-grey filter-type<?=($request['card_voice']['sex']==1?' max-grey':'') ?>" data-filter="card_voice[sex]" data-val="1">Для парней</a>
+            <a href="#" class="button-grey filter-type<?=(isset($request['card_voice']['sex']) && $request['card_voice']['sex']==2?' max-grey':'') ?>"  data-filter="card_voice[sex]" data-val="2">Для девушек</a>
+            <a href="#" class="button-grey filter-type<?=(isset($request['card_voice']['sex']) && $request['card_voice']['sex']==1?' max-grey':'') ?>" data-filter="card_voice[sex]" data-val="1">Для парней</a>
             <span class="count-card m-l-58">2528 открыток.</span>
             <div class="filter-how m-l-10">
                 <span>Сначала:&nbsp;</span>
