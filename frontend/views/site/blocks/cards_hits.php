f47f2026273f7ead5c28ce52849a81330a1f9ca2
Sergey <s.osipov@smsaero.ru>
2017-02-24 18:41:43 +0600

    test

--- frontend/views/site/blocks/cards_hits.php
+++ frontend/views/site/blocks/cards_hits.php
@@ -6,6 +6,8 @@ $css_style_categories = Yii::$app->params['css_style_categories'];
 
 $main_category = $model->mainCategory;
 
+var_dump($main_category);
+
 ?>
 <div class="player-min-content d-flex-s-b">
     <div class="player-min">
@@ -16,5 +18,5 @@ $main_category = $model->mainCategory;
         <a href="<?='/card/'.$url ?>" class="card-title m-b-12"><?=$model->page->card_name ? $model->page->card_name:'без названия' ?></a>
         <a href="<?='/card/'.$url ?>" class="button-yellow l-h-30">Отправить</a>
     </div>
-    <a class="link-to-category small-text <?=$css_style_categories[$main_category['id']] ?>" href="<?=Url::toRoute(['site/category', 'id' => $main_category['id']]) ?>"><?=$main_category['name'] ?></a>
+<!--    <a class="link-to-category small-text --><?//=$css_style_categories[$main_category['id']] ?><!--" href="--><?//=Url::toRoute(['site/category', 'id' => $main_category['id']]) ?><!--">--><?//=$main_category['name'] ?><!--</a>-->
 </div>
